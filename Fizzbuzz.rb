d0aecb04cb5ab6b34db9f19f7485fa396960982c
Kevin Minutti <minutti70@gmail.com>
2020-03-24 04:57:33 +0000

    Automatically backed up by Learn

---
+++ Fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(i)
+   if i % 15 == 0
+     puts "FizzBuzz"
+   elsif i % 5 == 0
+     puts "Buzz"
+   elsif i % 3 == 0
+     puts "Fizz"
+   else
+     puts i
+   end
+ end
+
+ fizzbuzz(3)
+ fizzbuzz(5)
+ fizzbuzz(15)
+ 
---
+++ restaurant.rb
@@ -0,0 +1,7 @@
+def restaurant
+    restaurant_name =
+    "Guy's American Kitchen & Bar"
+      cusisine = "american"
+      motto = "Welcome to Flavor Town"
+end
+puts restaurant
